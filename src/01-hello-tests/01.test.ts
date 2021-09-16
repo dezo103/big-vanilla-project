35bf4902d0bdad92bfe6ad3abf7cbf85fac2b51d
Aliaksandr Rudyk <dezo103@gmail.com>
2021-09-17 00:08:35 +0300

    feat: add test file (TDD)

---
+++ src/01-hello-tests/01.test.ts
@@ -0,0 +1,28 @@
+import {mult, sum} from "./01";
+
+let a: number;
+let b: number;
+let c: number;
+
+beforeEach(() => {
+    a = 1;
+    b = 2;
+    c = 3;
+})
+
+test('correct sum', () => {
+
+    const result1 = sum(a,b);
+    const result2 = sum(a,c);
+
+    expect(result1).toBe(3);
+    expect(result2).toBe(4);
+})
+
+test('correct mult', () => {
+
+    const result1 = mult(a,b);
+    const result2 = mult(b,c);
+
+    expect(result2).toBe(6);
+})
